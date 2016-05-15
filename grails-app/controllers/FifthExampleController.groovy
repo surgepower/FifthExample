7399c38f087aa180f71daeb288da29615e17c77a
powersurge <power.surge43@gmail.com>
2016-05-14 18:31:37 -0700

    Hi

---
+++ grails-app/controllers/FifthExampleController.groovy
@@ -0,0 +1,57 @@
+
+import fifthexample.FifthExample
+import metafunctionality.Module
+import metafunctionality.ModuleOutput
+
+
+class FifthExampleController {
+
+    def start() {
+        String inputID = Module.findByModuleId(params.id).inputID
+        FifthExample input = FifthExample.findByModuleDataID(inputID)
+        String word = input.word
+        List<String> rc = input.rhymingCandidates
+        rc.add(input.answer)
+        //rc.toSet()
+        [rc:rc, word:word, modID: params.id]
+    }
+
+    def submit() {
+
+        List<String> valueRows = new ArrayList<String>()
+        ModuleOutput output = new ModuleOutput()
+        output.headers = ["word_selected", "correct_answer", "result"]
+        String selected = params.candidates
+
+        String inputID = Module.findByModuleId(params.modID).inputID
+        FifthExample input = FifthExample.findByModuleDataID(inputID)
+
+        String answer = input.answer
+        String valueRow = selected + "," + answer + ","
+        if (selected.equals(answer)) {
+            valueRow += "correct"
+        } else {
+            valueRow += "incorrect"
+        }
+        valueRows.add(valueRow)
+        output.valueRows = valueRows
+        Module m = Module.findByModuleId(params.modID)
+        if (m.outputIDs != null) {
+            m.outputIDs.add(output.moduleDataID)
+        } else {
+            m.outputIDs = [output.moduleDataID]
+        }
+        output.type = "FifthExample"
+        m.isCompleted = true
+        m.save(flush: true)
+        output.save(flush: true)
+        render(view: "submit")
+        //redirect(controller: "appforliteracy.FileOutput", action: "output", params: [id: output.moduleDataID])
+
+    }
+
+    def logout() {
+        redirect(controller:"Login", action:"index")
+    }
+
+}
